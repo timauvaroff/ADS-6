010913229322d52730a4572aef679561c9a54175
Уваров Тимофей Максимович <66253074+timauvaroff@users.noreply.github.com>
2023-06-05 19:49:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -22,8 +22,7 @@ class TPQueue {
             res -= size + 1;
         return res;
     }
-    
-    public:
+  public:
     TPQueue() : begin(0), end(0), count(0) {
         arr = new T[size + 1];
     }
