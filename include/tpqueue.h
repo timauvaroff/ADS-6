a60ee835984a4957b9f67d79d3e20f218c5a0e7f
Уваров Тимофей Максимович <66253074+timauvaroff@users.noreply.github.com>
2023-06-05 19:33:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,55 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+  private:
+    T* arr;
+    int begin;
+    int end;
+    int count;
+    int step_back(int index) const
+    {
+        int res = --index;
+        if (res < 0)
+            res += size + 1;
+        return res;
+    }
+    int step_forward(int index) const
+    {
+        int res = ++index;
+        if (res > size)
+            res -= size + 1;
+        return res;
+    }
+public:
+    TPQueue() : begin(0), end(0), count(0)
+    {
+        arr = new T[size + 1];
+    }
+    ~TPQueue()
+    {
+        delete[] arr;
+    }
+    void push(const T& item)
+    {
+        assert(count < size);
+        int current = end;
+        while (current != begin && item.prior > arr[step_back(current)].prior)
+        {
+            arr[current] = arr[step_back(current)];
+            current = step_back(current);
+        }
+        arr[current] = item;
+        end = step_forward(end);
+        count++;
+    }
+    T pop()
+    {
+        assert(count > 0);
+        T item = arr[begin];
+        begin = step_forward(begin);
+        count--;
+        return item;
+    }
 
 struct SYM {
   char ch;
